81b7c3ee1f53164064e58ce2aa7955b3a5ef41b7
Sergio Bossa <sergio.bossa@gmail.com>
2011-05-21 15:49:39 +0200

    Improved metrics and related tests: now using variance rather than std deviation, and fixed bugs.

--- src/nimrod/core/metrics.clj
+++ src/nimrod/core/metrics.clj
@@ -10,23 +10,21 @@
 
 ; ---
 
-(defn- notify-gauge [gauge id value update-time]
+(defn- notify-gauge [gauge id timestamp value]
   (send gauge #(if-let [state %1]
-                 (conj state {:update update-time :value value})
-                 {:id id :start update-time :update update-time :value value}
+                 (conj state {:timestamp (Long/parseLong timestamp) :value value})
+                 {:id id :timestamp (Long/parseLong timestamp) :value value}
                  )
     )
   )
 
-(defn set-gauge [id value]
-  (let [update-time (date-in-millis)]
-    (dosync
-      (if-let [gauge (get @gauges id)]
-        (notify-gauge gauge id value update-time)
-        (let [gauge (new-agent nil)]
-          (alter gauges conj [id gauge])
-          (notify-gauge gauge id value update-time)
-          )
+(defn set-gauge [id timestamp value]
+  (dosync
+    (if-let [gauge (get @gauges id)]
+      (notify-gauge gauge id timestamp value)
+      (let [gauge (new-agent nil)]
+        (alter gauges conj [id gauge])
+        (notify-gauge gauge id timestamp value)
         )
       )
     )
@@ -34,51 +32,49 @@
 
 ; ---
 
-(defn- notify-counter [counter id value update-time]
+(defn- notify-counter [counter id timestamp value]
   (send counter #(if-let [state %1]
-                   (let [previous-time (state :update)
+                   (let [previous-time (state :timestamp)
                          previous-value (state :value)
                          previous-interval-average (state :interval-average)
                          previous-value-average (state :value-average)
-                         previous-interval-std-deviation (state :interval-std-deviation)
-                         previous-value-std-deviation (state :value-std-deviation)
-                         interval (- update-time previous-time)
+                         previous-interval-variance (state :interval-variance)
+                         previous-value-variance (state :value-variance)
+                         new-value (Long/parseLong value)
+                         interval (- (Long/parseLong timestamp) previous-time)
                          samples (inc (state :samples))
                          interval-average (average samples previous-interval-average interval)
-                         value-average (average samples previous-value-average value)
-                         interval-std-deviation (std-deviation samples previous-interval-std-deviation previous-interval-average interval-average interval)
-                         value-std-deviation (std-deviation samples previous-value-std-deviation previous-value-average value-average value)]
-                     (conj state {:update update-time
-                                  :value (+ previous-value value)
+                         value-average (average samples previous-value-average new-value)
+                         interval-variance (variance samples previous-interval-variance previous-interval-average interval-average interval)
+                         value-variance (variance samples previous-value-variance previous-value-average value-average new-value)]
+                     (conj state {:timestamp (Long/parseLong timestamp)
+                                  :value (+ previous-value new-value)
                                   :samples samples
                                   :interval-average interval-average
                                   :value-average value-average
-                                  :interval-std-deviation interval-std-deviation
-                                  :value-std-deviation value-std-deviation
+                                  :interval-variance interval-variance
+                                  :value-variance value-variance
                                   })
                      )
                    {:id id 
-                    :start update-time
-                    :update update-time
-                    :value value
+                    :timestamp (Long/parseLong timestamp)
+                    :value (Long/parseLong value)
                     :samples 1
-                    :interval-average 0
-                    :interval-std-deviation 0
-                    :value-average value
-                    :value-std-deviation 0}
+                    :interval-average (Long/parseLong timestamp)
+                    :interval-variance 0
+                    :value-average (Long/parseLong value)
+                    :value-variance 0}
                    )
     )
   )
 
-(defn set-counter [id value]
-  (let [update-time (date-in-millis)]
-    (dosync
-      (if-let [counter (get @counters id)]
-        (notify-counter counter id value update-time)
-        (let [counter (new-agent nil)]
-          (alter counters conj [id counter])
-          (notify-counter counter id value update-time)
-          )
+(defn set-counter [id timestamp value]
+  (dosync
+    (if-let [counter (get @counters id)]
+      (notify-counter counter id timestamp value)
+      (let [counter (new-agent nil)]
+        (alter counters conj [id counter])
+        (notify-counter counter id timestamp value)
         )
       )
     )
@@ -86,37 +82,47 @@
 
 ; ---
 
-(defn- notify-timer [timer id value]
+(defn- notify-timer [timer id timestamp value]
   (send timer #(if-let [state %1]
                  (if (= 0 (state :end))
                    (let [previous-elapsed-time-average (state :elapsed-time-average)
-                         previous-elapsed-time-std-deviation (state :elapsed-time-std-deviation)
+                         previous-elapsed-time-variance (state :elapsed-time-variance)
+                         new-value (Long/parseLong value)
                          start (state :start)
                          samples (inc (state :samples))
-                         elapsed-time (- value start)
+                         elapsed-time (- new-value start)
                          elapsed-time-average (average samples previous-elapsed-time-average elapsed-time)
-                         elapsed-time-std-deviation (std-deviation samples previous-elapsed-time-std-deviation previous-elapsed-time-average elapsed-time-average elapsed-time)]
-                     (conj state {:end value
+                         elapsed-time-variance (variance samples previous-elapsed-time-variance previous-elapsed-time-average elapsed-time-average elapsed-time)]
+                     (conj state {:timestamp (Long/parseLong timestamp)
+                                  :end new-value
                                   :elapsed-time elapsed-time
                                   :elapsed-time-average elapsed-time-average
-                                  :elapsed-time-std-deviation elapsed-time-std-deviation
+                                  :elapsed-time-variance elapsed-time-variance
                                   :samples samples})
                      )
-                   (conj state {:start value :end 0 :update 0 :elapsed-time 0})
+                   (conj state {:timestamp (Long/parseLong timestamp) :start (Long/parseLong value) :end 0 :elapsed-time 0})
                    )
-                 {:id id :start value :end 0 :elapsed-time 0 :elapsed-time-average 0 :elapsed-time-std-deviation 0 :samples 0}
+                 {:id id :timestamp (Long/parseLong timestamp) :start (Long/parseLong value) :end 0 :elapsed-time 0 :elapsed-time-average 0 :elapsed-time-variance 0 :samples 0}
                  )
     )
   )
 
-(defn set-timer [id value]
+(defn set-timer [id timestamp value]
   (dosync
     (if-let [timer (get @timers id)]
-      (notify-timer timer id value)
+      (notify-timer timer id timestamp value)
       (let [timer (new-agent nil)]
         (alter timers conj [id timer])
-        (notify-timer timer id value)
+        (notify-timer timer id timestamp value)
         )
       )
     )
   )
+
+; ---
+
+(defonce metrics {
+                  "gauge" set-gauge
+                  "counter" set-counter
+                  "timer" set-timer
+                  })
