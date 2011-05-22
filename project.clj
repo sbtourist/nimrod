eafc2513aca2834a5d976558faf2ef0debf94442
Sergio Bossa <sergio.bossa@gmail.com>
2011-05-22 11:31:14 +0200

    Removed unused functions and deps.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
  :dependencies [
                 [org.clojure/clojure "1.2.1"]
                 [org.clojure/clojure-contrib "1.2.0"]
-                [clj-time "0.3.0"]
                 [commons-io/commons-io "2.0.1"]
                 ]
  ;jvm-opts ["-agentlib:jdwp=transport=dt_socket,address=9000,server=y,suspend=n"]
--- src/nimrod/core/util.clj
+++ src/nimrod/core/util.clj
@@ -3,14 +3,6 @@
  (:import [java.util Date])
  )
 
-(defn date []
-  (Date.)
-  )
-
-(defn date-in-millis []
-  (.getTime (Date.))
-  )
-
 (defn new-agent [state]
   (let [a (agent state)]
     (set-error-handler! a #(log/log :error (.getMessage %2) %2))
