1a6ca98630f13ca7acbbe5c39400c262fdc50374
Sergio Bossa <sergio.bossa@gmail.com>
2011-05-22 18:36:23 +0200

    Avoiding to repeteadly call Long/parseLong in metrics + imporved tests.

--- src/nimrod/core/metrics.clj
+++ src/nimrod/core/metrics.clj
@@ -33,38 +33,39 @@
 ; ---
 
 (defn- notify-counter [counter id timestamp value]
-  (send counter #(if-let [state %1]
-                   (let [previous-time (state :timestamp)
-                         previous-value (state :value)
-                         previous-interval-average (state :interval-average)
-                         previous-value-average (state :value-average)
-                         previous-interval-variance (state :interval-variance)
-                         previous-value-variance (state :value-variance)
-                         new-value (Long/parseLong value)
-                         interval (- (Long/parseLong timestamp) previous-time)
-                         samples (inc (state :samples))
-                         interval-average (average samples previous-interval-average interval)
-                         value-average (average samples previous-value-average new-value)
-                         interval-variance (variance samples previous-interval-variance previous-interval-average interval-average interval)
-                         value-variance (variance samples previous-value-variance previous-value-average value-average new-value)]
-                     (conj state {:timestamp (Long/parseLong timestamp)
-                                  :value (+ previous-value new-value)
-                                  :samples samples
-                                  :interval-average interval-average
-                                  :value-average value-average
-                                  :interval-variance interval-variance
-                                  :value-variance value-variance
-                                  })
+  (let [n_timestamp (Long/parseLong timestamp) n_value (Long/parseLong value)]
+    (send counter #(if-let [state %1]
+                     (let [previous-time (state :timestamp)
+                           previous-value (state :value)
+                           previous-interval-average (state :interval-average)
+                           previous-value-average (state :value-average)
+                           previous-interval-variance (state :interval-variance)
+                           previous-value-variance (state :value-variance)
+                           interval (- n_timestamp previous-time)
+                           samples (inc (state :samples))
+                           interval-average (average samples previous-interval-average interval)
+                           value-average (average samples previous-value-average n_value)
+                           interval-variance (variance samples previous-interval-variance previous-interval-average interval-average interval)
+                           value-variance (variance samples previous-value-variance previous-value-average value-average n_value)]
+                       (conj state {:timestamp n_timestamp
+                                    :value (+ previous-value n_value)
+                                    :samples samples
+                                    :interval-average interval-average
+                                    :value-average value-average
+                                    :interval-variance interval-variance
+                                    :value-variance value-variance
+                                    })
+                       )
+                     {:id id
+                      :timestamp n_timestamp
+                      :value n_value
+                      :samples 1
+                      :interval-average n_timestamp
+                      :interval-variance 0
+                      :value-average n_value
+                      :value-variance 0}
                      )
-                   {:id id 
-                    :timestamp (Long/parseLong timestamp)
-                    :value (Long/parseLong value)
-                    :samples 1
-                    :interval-average (Long/parseLong timestamp)
-                    :interval-variance 0
-                    :value-average (Long/parseLong value)
-                    :value-variance 0}
-                   )
+      )
     )
   )
 
@@ -83,28 +84,29 @@
 ; ---
 
 (defn- notify-timer [timer id timestamp value]
+  (let [n_timestamp (Long/parseLong timestamp) n_value (Long/parseLong value)]
   (send timer #(if-let [state %1]
                  (if (= 0 (state :end))
                    (let [previous-elapsed-time-average (state :elapsed-time-average)
                          previous-elapsed-time-variance (state :elapsed-time-variance)
-                         new-value (Long/parseLong value)
                          start (state :start)
                          samples (inc (state :samples))
-                         elapsed-time (- new-value start)
+                         elapsed-time (- n_value start)
                          elapsed-time-average (average samples previous-elapsed-time-average elapsed-time)
                          elapsed-time-variance (variance samples previous-elapsed-time-variance previous-elapsed-time-average elapsed-time-average elapsed-time)]
-                     (conj state {:timestamp (Long/parseLong timestamp)
-                                  :end new-value
+                     (conj state {:timestamp n_timestamp
+                                  :end n_value
                                   :elapsed-time elapsed-time
                                   :elapsed-time-average elapsed-time-average
                                   :elapsed-time-variance elapsed-time-variance
                                   :samples samples})
                      )
-                   (conj state {:timestamp (Long/parseLong timestamp) :start (Long/parseLong value) :end 0 :elapsed-time 0})
+                   (conj state {:timestamp n_timestamp :start n_value :end 0 :elapsed-time 0})
                    )
-                 {:id id :timestamp (Long/parseLong timestamp) :start (Long/parseLong value) :end 0 :elapsed-time 0 :elapsed-time-average 0 :elapsed-time-variance 0 :samples 0}
+                 {:id id :timestamp n_timestamp :start n_value :end 0 :elapsed-time 0 :elapsed-time-average 0 :elapsed-time-variance 0 :samples 0}
                  )
     )
+  )
   )
 
 (defn set-timer [id timestamp value]
