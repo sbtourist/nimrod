ee8774df16e493b2971cd8a0fd92d578abdfc2c6
Sergio Bossa <sergio.bossa@gmail.com>
2011-06-09 14:19:54 +0200

    Added CORS support for GET requests.

--- src/nimrod/web/app.clj
+++ src/nimrod/web/app.clj
@@ -11,13 +11,24 @@
  )
 
 (defonce response-codes {:ok 200 :no-content 204 :not-found 404 :error 500})
+(defonce response-headers {"Content-Type" "application/json"})
+(defonce cors-response-headers {"Content-Type" "application/json" "Access-Control-Allow-Origin" "*"})
+
+(defn- cors-response 
+  ([status body]
+    {:headers cors-response-headers :status (response-codes status) :body (json/json-str body)}
+    )
+  ([status]
+    {:headers cors-response-headers :status (response-codes status)}
+    )
+  )
 
 (defn- response 
   ([status body]
-    {:headers {"Content-Type" "application/json"} :status (response-codes status) :body (json/json-str body)}
+    {:headers response-headers :status (response-codes status) :body (json/json-str body)}
     )
   ([status]
-    {:headers {"Content-Type" "application/json"} :status (response-codes status)}
+    {:headers response-headers :status (response-codes status)}
     )
   )
 
@@ -39,7 +50,7 @@
       )
     )
   (http/GET "/logs" []
-    (response :ok (list-tailers))
+    (cors-response :ok (list-tailers))
     )
   (http/DELETE "/logs/:log-id" [log-id]
     (stop-tailer log-id)
@@ -48,19 +59,19 @@
   (http/GET "/logs/:log-id/:metric-type" [log-id metric-type]
     (if-let [metric (metric-types (keyword metric-type))]
       (if-let [result (list-metrics metric log-id)]
-        (response :ok result)
-        (response :not-found)
+        (cors-response :ok result)
+        (cors-response :not-found)
         )
-      (response :error {:error (str "Bad metric type: " metric-type)})
+      (cors-response :error {:error (str "Bad metric type: " metric-type)})
       )
     )
   (http/GET ["/logs/:log-id/:metric-type/:metric-id" :metric-id #"[^/?#]+"] [log-id metric-type metric-id]
     (if-let [metric (metric-types (keyword metric-type))]
       (if-let [result (read-metric metric log-id metric-id)]
-        (response :ok result)
-        (response :not-found)
+        (cors-response :ok result)
+        (cors-response :not-found)
         )
-      (response :error {:error (str "Bad metric type: " metric-id)})
+      (cors-response :error {:error (str "Bad metric type: " metric-id)})
       )
     )
   (http/POST ["/logs/:log-id/:metric-type/:metric-id/history" :metric-id #"[^/?#]+"] [log-id metric-type metric-id limit]
@@ -75,19 +86,19 @@
   (http/GET ["/logs/:log-id/:metric-type/:metric-id/history" :metric-id #"[^/?#]+"] [log-id metric-type metric-id]
     (if-let [metric (metric-types (keyword metric-type))]
       (if-let [result (read-history metric log-id metric-id nil)]
-        (response :ok result)
-        (response :not-found)
+        (cors-response :ok result)
+        (cors-response :not-found)
         )
-      (response :error {:error (str "Bad metric type: " metric-id)})
+      (cors-response :error {:error (str "Bad metric type: " metric-id)})
       )
     )
   (http/GET ["/logs/:log-id/:metric-type/:metric-id/history/:tags" :metric-id #"[^/?#]+" :tags #"[^/?#]+"] [log-id metric-type metric-id tags]
     (if-let [metric (metric-types (keyword metric-type))]
       (if-let [result (read-history metric log-id metric-id (into #{} (string/split tags #",")))]
-        (response :ok result)
-        (response :not-found)
+        (cors-response :ok result)
+        (cors-response :not-found)
         )
-      (response :error {:error (str "Bad metric type: " metric-id)})
+      (cors-response :error {:error (str "Bad metric type: " metric-id)})
       )
     )
   (route/not-found "")
