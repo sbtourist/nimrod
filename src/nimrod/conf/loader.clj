f9bc18d9ffea773b9131b08b519840545fd8b7a0
Sergio Bossa <sergio.bossa@gmail.com>
2011-06-09 14:19:31 +0200

    Throwing exception if trying to register a wrongly configured log.

--- src/nimrod/conf/loader.clj
+++ src/nimrod/conf/loader.clj
@@ -10,10 +10,15 @@
     (if-let [logs-property (.getProperty props "nimrod.logs")]
       (loop [logs (string/split logs-property #",")]
         (let [log-data (string/split (first logs) #":")]
-          (start-tailer (log-data 0) (Long/parseLong (log-data 1)))
-          (if-let [other-logs (seq (rest logs))]
-            (recur other-logs)
-            nil
+          (if (= 2 (count log-data))
+            (do
+              (start-tailer (log-data 0) (Long/parseLong (log-data 1)))
+              (if-let [other-logs (seq (rest logs))]
+                (recur other-logs)
+                nil
+                )
+              )
+            (throw (IllegalStateException. (str "Bad logs configuration: " log-data)))
             )
           )
         )
