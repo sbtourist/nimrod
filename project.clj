0cdfc3aea59ee871b04363d60b5634698e0c8833
Sergio Bossa <sergio.bossa@gmail.com>
2012-08-10 08:10:31 +0100

    Upgraded hsqldb to 2.2.9 final.

--- project.clj
+++ project.clj
@@ -10,9 +10,9 @@
                 [ring/ring-jetty-adapter "1.1.0"]
                 [com.typesafe.config/config "0.2.0"]
                 [tayler/tayler "1.1"]
-                [org.hsqldb/hsqldb "2.2.9-SNAPSHOT"]
+                [org.hsqldb/hsqldb "2.2.9"]
                 [org.slf4j/slf4j-api "1.6.1"]
                 [org.slf4j/slf4j-log4j12 "1.6.1"]]
- :repositories {"typesafe" "http://repo.typesafe.com/typesafe/releases"}
+ :repositories {"hsqldb" "http://www.hsqldb.org/repos" "typesafe" "http://repo.typesafe.com/typesafe/releases"}
  :aot [nimrod.core.startup]
  :java-source-path "src")
